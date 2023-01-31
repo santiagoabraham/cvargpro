8170ebd87931cbe78ed637afc29afca020e564af
Santiago Abraham <santiago.abraham2611@gmail.com>
2023-01-31 20:12:42 -0300

    Primer escritura en el repositorio

---
+++ Ticmas/js/function.js
@@ -0,0 +1,16 @@
+document.getElementById('boton_ocultar').addEventListener('click', function() {
+    document.getElementById('demo').style.display = 'none';
+});
+
+document.getElementById('boton_ocultar').addEventListener('click', function() {
+    document.getElementById('boton_ocultar').style.display = 'none';
+});
+
+document.getElementById('boton_ocultar').addEventListener('click', function() {
+    document.getElementById('muestra').style.display = 'block';
+});
+
+/*document.addEventListener('click', function() {
+    console.log("Perdiste");
+
+});*/
